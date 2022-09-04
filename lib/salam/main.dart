ff824068da410138cd5a9a22d13e457714470ea6
Salam <salam73@gmail.com>
2022-09-04 15:58:08 +0300

    update project

--- lib/salam/main.dart
+++ lib/salam/main.dart
@@ -1,18 +1,28 @@
-void main(){
-Student s=Student();
-s.me='haydla';
-print(s.me);
-s.nr=14;
-print(s.nr);
+import 'dart:io';
+
+void main() {
+  String? name = stdin.readLineSync();
+  String f=name!;
+  print("Hello, $f");
+  Student s = Student();
+  s.me = 'haydla';
+  print(s.me);
+  s.nr = 14;
+  print(s.nr);
 }
 
-class Student{
-String ?f;
-late int d;
-   set me(String m) {f=m;}
-   String get me=> f!.contains('la')?'la in $f':'la not in $f';
+class Student {
+  String? f;
+  late int d;
+  set me(String m) {
+    f = m;
+  }
 
-   set nr(int n){d=n*10;}
+  String get me => f!.contains('la') ? 'la in $f' : 'la not in $f';
 
-int get nr=> d;
+  set nr(int n) {
+    d = n * 10;
+  }
+
+  int get nr => d;
 }
