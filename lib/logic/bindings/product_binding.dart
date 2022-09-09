fe9cfc14207111a64a634bb4388f6336f0580e7c
Salam <salam73@gmail.com>
2022-09-09 20:19:20 +0300

    add product_binding.dart file to project

---
+++ lib/logic/bindings/product_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/instance_manager.dart';
+
+import '../controllers/product_controller.dart';
+
+class ProductBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(ProductController());
+
+  }
+}
